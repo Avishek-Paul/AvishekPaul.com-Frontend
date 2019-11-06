c0e5e30263cdc36b2ab8b7f3be07bb68d1bc9b81
Avishek Paul <avishek97paul@gmail.com>
2019-11-06 00:09:53 -0500

    fixed about section on smaller screens using media rule

--- src/components/layout/Sections/About/AboutImageSlide.js
+++ src/components/layout/Sections/About/AboutImageSlide.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 import XandrAboutImage from "./images/xandr_about.jpg";
 import BinghamtonAboutImage from "./images/binghamton_about.jpg";
@@ -7,28 +7,53 @@ import CodeAboutImage from "./images/generic_code.png";
 export class AboutImageSlide extends Component {
   render() {
     return (
-      <div className="AboutImageRow">
-        <img className="AboutImage" alt="Xandr Logo" src={XandrAboutImage} />
-        <img
-          className="AboutImage"
-          alt="Binghamton University Logo"
-          src={BinghamtonAboutImage}
-        />
-        <img className="AboutImage" alt="Generic Code" src={CodeAboutImage} />
-        <p className="AboutImageCaption">
-          I work as a <strong>Services Rotational Associate</strong> at{" "}
-          <strong>Xandr</strong>. My Current title is{" "}
-          <strong>Product Support Specialist</strong>
-        </p>
-        <p className="AboutImageCaption">
-          I graduated from <strong>Binghamton University</strong> with a{" "}
-          <i>BS</i> in <strong>Computer Engineering</strong>.
-        </p>
-        <p className="AboutImageCaption">
-          I'm a very <strong>analytical</strong> person. I love working with{" "}
-          <strong>numbers</strong> and <strong>code</strong>.
-        </p>
-      </div>
+      <Fragment>
+        <div className="AboutImageRow GreaterThan775">
+          <img className="AboutImage" alt="Xandr Logo" src={XandrAboutImage} />
+          <img
+            className="AboutImage"
+            alt="Binghamton University Logo"
+            src={BinghamtonAboutImage}
+          />
+          <img className="AboutImage" alt="Generic Code" src={CodeAboutImage} />
+          <p className="AboutImageCaption">
+            I work as a <strong>Services Rotational Associate</strong> at{" "}
+            <strong>Xandr</strong>. My Current title is{" "}
+            <strong>Product Support Specialist</strong>
+          </p>
+          <p className="AboutImageCaption">
+            I graduated from <strong>Binghamton University</strong> with a{" "}
+            <i>BS</i> in <strong>Computer Engineering</strong>.
+          </p>
+          <p className="AboutImageCaption">
+            I'm a very <strong>analytical</strong> person. I love working with{" "}
+            <strong>numbers</strong> and <strong>code</strong>.
+          </p>
+        </div>
+
+        <div className="AboutImageRow LessThan775">
+          <img className="AboutImage" alt="Xandr Logo" src={XandrAboutImage} />{" "}
+          <p className="AboutImageCaption">
+            I work as a <strong>Services Rotational Associate</strong> at{" "}
+            <strong>Xandr</strong>. My Current title is{" "}
+            <strong>Product Support Specialist</strong>
+          </p>
+          <img
+            className="AboutImage"
+            alt="Binghamton University Logo"
+            src={BinghamtonAboutImage}
+          />
+          <p className="AboutImageCaption">
+            I graduated from <strong>Binghamton University</strong> with a{" "}
+            <i>BS</i> in <strong>Computer Engineering</strong>.
+          </p>
+          <img className="AboutImage" alt="Generic Code" src={CodeAboutImage} />
+          <p className="AboutImageCaption">
+            I'm a very <strong>analytical</strong> person. I love working with{" "}
+            <strong>numbers</strong> and <strong>code</strong>.
+          </p>
+        </div>
+      </Fragment>
     );
   }
 }
